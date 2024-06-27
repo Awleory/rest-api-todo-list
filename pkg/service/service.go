4a3346927f2a0dbb964400492182b3c67e508d17
Awleory <llordtrane@gmail.com>
2024-06-27 08:47:22 +0300

    users sign up

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,8 +1,12 @@
 package service
 
-import "github.com/Awleory/todo-app/pkg/repository"
+import (
+	"github.com/Awleory/todo-app"
+	"github.com/Awleory/todo-app/pkg/repository"
+)
 
 type Authorization interface {
+	CreateUser(todo.User) (int, error)
 }
 
 type TodoList interface {
@@ -18,5 +22,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
